29c7efe6abd6f01cdd5ca6a3ec33571edfc5452e
Anicet Ebou <anicet.ebou@gmail.com>
2019-11-17 00:13:33 +0000

    add merge option to tidy_bunch

--- R/tidy_bunch.R
+++ R/tidy_bunch.R
@@ -12,44 +12,53 @@
 #   https://www.gapminder.org/data/
 #
 
-tidy_bunch <- function(path = ".", recursive = FALSE) {
+tidy_bunch <- function(path = ".", merge = FALSE) {
 
   # Data import ---------------------------------------
 
-  # list files in indicated directory
-  file_list <- list.files(path, pattern = "[.](xls|xlsx)$", full.names = TRUE, recursive = recursive)
+  # Gapminder indices files souhl be in the same directory
+  # to be grouped under a same indice group.
+  file_list <- list.files(path, pattern = "[.](xls|xlsx)$", full.names = TRUE)
 
-  # read listed file in a data frame list
+  # To read all files once
   df_list <- lapply(file_list, readxl::read_excel)
 
   # Data transformation -------------------------------
 
-  # initialise empty vector
   desc_vec <- c()
 
   # Typically, gapminder data sheets contains indicator description in
   # first cell of data sheet. We read this description to use it further
-  # as the column name of the indicator
+  # as the column name of the indicator.
   for (df in df_list) {
     desc_vec <- c(desc_vec, colnames(df)[1])
   }
 
   # Core tidy up: we take the dataframe in list and assign
   # the country column name, and tidy it up using tidyr
-  # appropriate function and change year column to numeric
+  # appropriate function and change year column to numeric.
   df_list <- lapply(df_list, function(df) {
     names(df)[1] <- "country"
-    df <- df %>% dplyr::gather(year, indice, -country)
+    df <- df %>% tidyr::gather(year, indice, -country)
     df$year <- as.numeric(df$year)
     df
   })
 
-  # We now loop through the desc_vec to assign the right name
-  # of the indicator to the right data frame at the right column
+  # Assign the right name of the indicator to the right
+  # data frame at the right column.
   for (i in 1:length(desc_vec)) {
     names(df_list[[i]])[3] <- desc_vec[i]
   }
 
   # Return result ------------------------------------
-  df_list
+
+  # Check if the user enable data frames to be merged into one
+  # or not.
+  if (merge == TRUE){
+    df <- dplyr::bind_rows(df_list)
+    df
+  } else {
+    df_list
+  }
+
 }
