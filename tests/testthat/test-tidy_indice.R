a703a19ef635375850cb74b50e64e505e06854fc
Anicet Ebou <anicet.ebou@gmail.com>
2019-11-19 14:53:25 +0000

    add pkgdown

--- tests/testthat/test-tidy_indice.R
+++ tests/testthat/test-tidy_indice.R
@@ -1,9 +1,6 @@
 df <- system.file("extdata", "children_per_woman_total_fertility.xlsx",
                   package = "tidygapminder")
 
-txt <- system.file("extdata", "testfile.txt",
-                  package = "tidygapminder")
-
 test_that("Output a data frame", {
   expect_true(is.data.frame(tidy_indice(df)))
 })
@@ -13,6 +10,3 @@ test_that("Colnames are right", {
                c("country", "year", "children_per_woman_total_fertility"))
 })
 
-test_that("Output error if ext is not csv or xlsx", {
-  expect_error(tidy_indice(txt))
-})
